8ec0e77706233199dd3c645e7fb5348771c71e67
Kroustille <6004961+Kroustille@users.noreply.github.com>
2025-06-07 13:52:56 +0200

    feat: add navigation to rabbits and capture meal

---
+++ apps/web/src/module/ui/nav.tsx
@@ -0,0 +1,27 @@
+import React, { useMemo } from 'react'
+import { useRabbitsQuery } from '../rabbit-profile/query'
+import { Link } from 'react-router-dom'
+
+export const Nav: React.FC = () => {
+  const { data: rabbits } = useRabbitsQuery()
+
+  const rabbits_list = useMemo(() => {
+    return (rabbits ??[]).map(rabbit => <li key={rabbit.id}>
+        <Link to={`/rabbit/${rabbit.id}`}>
+          {rabbit.name}
+        </Link>
+      </li>
+    )
+  }, [rabbits])
+  
+  return <nav>
+    <h1>Navigation</h1>
+    <h2>Lapins</h2>
+    <ul>{rabbits_list}</ul>
+
+    <h2>Fonctionnalités</h2>
+    <ul>
+      <li><Link to="/capture-meal">Enregistrer un repas</Link></li>
+    </ul>
+  </nav>
+}
--- apps/web/src/root.tsx
+++ apps/web/src/root.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Nav } from './module/ui/nav'
 
 export const Root: React.FC = () => {
-  return <Outlet />
+  return <>
+    <Nav />
+    <main>
+      <Outlet />
+    </main>
+  </>
 }
