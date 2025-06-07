b10b7db0fc75c2aecc1f1e8ff5b2ccfc99f5f409
Kroustille <6004961+Kroustille@users.noreply.github.com>
2025-06-07 12:35:22 +0200

    tech: set web client api url from env variables

--- apps/web/src/client.ts
+++ apps/web/src/client.ts
@@ -1,5 +1,5 @@
 import { PinouClient } from '@pinou/client'
 
-const BASE_URL = import.meta.env.PROD ? 'http://83.228.197.122/api' : 'http://localhost:3000'
+const BASE_URL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : 'http://localhost:3000'
 
 export const client = new PinouClient(BASE_URL)
